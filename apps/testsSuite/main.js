c850d35da7f12b8ccf66bb236482a22c157c7728
salboaie <salboaie@gmail.com>
2013-04-06 19:28:43 +0300

    cleanings

--- apps/testsSuite/main.js
+++ apps/testsSuite/main.js
@@ -2,3 +2,8 @@
 function shapeMain(){
     return testRegistry;
 }
+
+<!--shape.registerShapeURL("testsSuite","view/testsSuite.html");
+shape.registerShapeURL("TestDescription","view/testDescription.html");
+shape.registerShapeURL("string","view/traceDescription.html");
+-->
--- apps/testsSuite/shapes.js
+++
@@ -1,4 +0,0 @@
-<!--shape.registerShapeURL("testsSuite","view/testsSuite.html");
-shape.registerShapeURL("TestDescription","view/testDescription.html");
-shape.registerShapeURL("string","view/traceDescription.html");
--->
