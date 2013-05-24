dbbc391faabc826b175b90353eff20d534178bdf
Marius Ciocan <marius.ciocan@trp.ro>
2013-05-24 19:59:43 +0300

    DbPersistence added

--- apps/testsSuite/tests/ServerPersistenceTest.js
+++ apps/testsSuite/tests/ServerPersistenceTest.js
@@ -1,29 +1,74 @@
 registerTest("Swarm Persistence",
     function () {
 
-        swarmClient = new SwarmClient("127.0.0.1", 2000, "mac", "ok", "macTenant", "testCtor");
-
-        swarmClient.addCallback("PersistenceManager.js", function (data) {
-            console.log("result1");
-            console.log(data);
-        });
-
-
-        var selectQueryReq = {};
-        selectQueryReq.type = "QUERY";
-        selectQueryReq.persistence = "DbPersistence";
-        selectQueryReq.className = "defaultMembersModel";
-        selectQueryReq.params = {
-            query: "select * from defaultMembersModel"
+        this.model = {
+            name: "SpaghettiMonster",
+            description: {
+                name: {
+                    type: "string"
+                },
+                father: {
+                    type: "SpaghettiMonster",
+                    value: null
+                },
+                family: {
+                    type: "collection",
+                    contains: "SpaghettiMonster"
+                },
+                prankVictim: {
+                    type: "SpaghettiMonster",
+                    value: null
+                }
+            }
         };
 
-        swarmClient.startSwarm("PersistenceManager.js", "processRequest", selectQueryReq);
-        this.startTesting(1, 100);
+        shape.setPersistenceForClass("SpaghettiMonster", "db");
+        this.persistence = BasePersistence.prototype.getPersistenceByName('db');
+        this.startTesting(1, 5000);
     },
     function () {
-        this.assert.equal(true, true);
+        test = {
+            createTable: function () {
+                self.persistence.create(self.model.name, self.model.description);
+            },
+            put: function (data) {
+                grandpa = shape.lookup("SpaghettiMonster", 1);
+                grandpa.name = "Luke's father";
+                self.persistence.put(grandpa.getClassName(), grandpa.getInnerValues());
+            },
+            update: function (data) {
+                grandpa.setPK(data.result.id);
+                grandpa.father = shape.newEntity("SpaghettiMonster", 1, grandpa);
+                grandpa.createMember('family');
+                grandpa.family.push(shape.newEntity("SpaghettiMonster", 2, grandpa));
+                grandpa.family.push(shape.newEntity("SpaghettiMonster", 3, grandpa));
+                grandpa.family.push(shape.newEntity("SpaghettiMonster", 5, grandpa));
+                self.persistence.update(grandpa.getClassName(), grandpa.getPK(), grandpa.getInnerValues());
+            },
+            getObject: function (data) {
+                self.persistence.get(grandpa.getClassName(), grandpa.getPK());
+            },
+            done: function (data) {
+                self.assert.equal(true, true);
+            }
+        };
+        var grandpa, father;
+        var self = this;
+
+        var steps = [test.put, test.update, test.getObject, test.getObject, test.done];
+
+        self.persistence.on('REFRESH', function (event) {
+            var result = event ? event.data : event;
+            console.log(result);
+            if (steps && steps.length) {
+                steps.shift()(event.data);
+            }
+        });
+
+        test.createTable();
     },
     function () {
+        this.persistence.drop(this.model.name);
     }
 )
 
---
+++ core/persistences/dbPersistence.js
@@ -0,0 +1,126 @@
+/**********************************************************************************************
+ * DataBasePersistence
+ **********************************************************************************************/
+function DbPersistence(spaceName) {
+    var dbPersistenceSwarmName = "PersistenceManager.js";
+    var dbPersistenceName = "DbPersistence";
+    var swarmClient;
+    var commands = [];
+    var self = this;
+    var customHandlers = {};
+    var timerId = null;
+
+    var init = function () {
+        makeEventEmitter(self);
+
+        //TODO : get swarmClientInstance from another class( ex: AuthenticationManager )
+        swarmClient = new SwarmClient("127.0.0.1", 2000, "mac", "ok", "macTenant", "testCtor");
+        swarmClient.addCallback(dbPersistenceSwarmName, function (data) {
+            self.proccessChange(data);
+        });
+
+
+        //TODO : refactor this somehow
+        customHandlers['create'] = onCreate;
+        customHandlers['drop'] = onDrop;
+        customHandlers['update'] = onUpdate;
+        customHandlers['delete'] = onDelete;
+        customHandlers['put'] = onPut;
+        customHandlers['get'] = onGet;
+    }
+
+
+    this.proccessChange = function (data) {
+        var requestHandler = customHandlers[data.request.type.toLowerCase()];
+        if (requestHandler) {
+            try {
+                requestHandler(data);
+            } catch (e) {
+            }
+        }
+        self.emit({type: 'REFRESH', data: data});
+    }
+
+
+    this.create = function (tableName, tableDescription) {
+        genericCall('create', tableName, {description: tableDescription});
+    }
+    var onCreate = function (data) {
+    }
+
+    this.drop = function (tableName) {
+        genericCall('drop', tableName);
+    }
+    var onDrop = function (data) {
+    }
+
+
+    this.put = function (tableName, data) {
+        genericCall('put', tableName, {data: data});
+    }
+    var onPut = function (data) {
+    }
+
+
+    this.update = function (tableName, id, data) {
+        genericCall('update', tableName, {id: id, data: data});
+    }
+    var onUpdate = function (data) {
+    }
+
+
+    this.get = function (tableName, id) {
+        genericCall('get', tableName, {id: id});
+    }
+    var onGet = function (data) {
+    }
+
+
+    this.delete = function (tableName, id, data) {
+        genericCall('delete', tableName, {id: id, data: data});
+    }
+    var onDelete = function (data) {
+    }
+
+
+    this.query = function (tableName, query) {
+        genericCall('query', tableName, {query: query});
+    }
+    var onQuery = function (data) {
+    }
+
+
+    var genericCall = function (callType, tableName, params) {
+        var req = getRequestObject(tableName, callType);
+        req.params = params;
+        commands.push(req);
+
+        clearInterval(timerId);
+        timerId = setInterval(runCommands, 100);
+    }
+
+
+    var getRequestObject = function (className, type) {
+        return request = {
+            type: type.toUpperCase(),
+            className: className,
+            persistence: dbPersistenceName
+        };
+    }
+
+
+    var runCommands = function () {
+        if (commands.length) {
+            swarmClient.startSwarm(dbPersistenceSwarmName, "processRequest", commands);
+        }
+        commands = [];
+        clearInterval(timerId);
+        timerId = null;
+    }
+
+
+    init();
+}
+
+DbPersistence.prototype = new BasePersistence();
+BasePersistence.prototype.registerPersistence("db", new DbPersistence());
