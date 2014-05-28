2913557a69b46cf35f16bf3d6100a1deee602e36
Dragos Oprescu <dragos.oprescu@trp.ro>
2014-05-28 14:09:20 +0300

    add code for phoneGap

--- deps/detectBrowser.js
+++ deps/detectBrowser.js
@@ -1,117 +1,116 @@
-
-var objappVersion = navigator.appVersion;
-var objAgent = navigator.userAgent;
-var objbrowserName  = navigator.appName;
-var objfullVersion  = ''+parseFloat(navigator.appVersion);
-var objBrMajorVersion = parseInt(navigator.appVersion,10);
-var objOffsetName,objOffsetVersion,ix;
+var app = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+if ( !app ) {
+    var objappVersion = navigator.appVersion;
+    var objAgent = navigator.userAgent;
+    var objbrowserName = navigator.appName;
+    var objfullVersion = '' + parseFloat(navigator.appVersion);
+    var objBrMajorVersion = parseInt(navigator.appVersion, 10);
+    var objOffsetName, objOffsetVersion, ix;
 
 // In Chrome
-if ((objOffsetVersion=objAgent.indexOf("Chrome"))!=-1) {
-    objbrowserName = "Chrome";
-    objfullVersion = objAgent.substring(objOffsetVersion+7);
-}
+    if ((objOffsetVersion = objAgent.indexOf("Chrome")) != -1) {
+        objbrowserName = "Chrome";
+        objfullVersion = objAgent.substring(objOffsetVersion + 7);
+    }
 // In Microsoft internet explorer
-else if ((objOffsetVersion=objAgent.indexOf("MSIE"))!=-1) {
-    objbrowserName = "Microsoft Internet Explorer";
-    objfullVersion = objAgent.substring(objOffsetVersion+5);
-}
+    else if ((objOffsetVersion = objAgent.indexOf("MSIE")) != -1) {
+        objbrowserName = "Microsoft Internet Explorer";
+        objfullVersion = objAgent.substring(objOffsetVersion + 5);
+    }
 
 // In Firefox
-else if ((objOffsetVersion=objAgent.indexOf("Firefox"))!=-1) {
-    objbrowserName = "Firefox";
-}
+    else if ((objOffsetVersion = objAgent.indexOf("Firefox")) != -1) {
+        objbrowserName = "Firefox";
+    }
 // In Safari
-else if ((objOffsetVersion=objAgent.indexOf("Safari"))!=-1) {
-    objbrowserName = "Safari";
-    objfullVersion = objAgent.substring(objOffsetVersion+7);
-    if ((objOffsetVersion=objAgent.indexOf("Version"))!=-1)
-        objfullVersion = objAgent.substring(objOffsetVersion+8);
-}
+    else if ((objOffsetVersion = objAgent.indexOf("Safari")) != -1) {
+        objbrowserName = "Safari";
+        objfullVersion = objAgent.substring(objOffsetVersion + 7);
+        if ((objOffsetVersion = objAgent.indexOf("Version")) != -1)
+            objfullVersion = objAgent.substring(objOffsetVersion + 8);
+    }
 
 // For other browser "name/version" is at the end of userAgent
-else if ( (objOffsetName=objAgent.lastIndexOf(' ')+1) <
-    (objOffsetVersion=objAgent.lastIndexOf('/')) )
-{
-    objbrowserName = objAgent.substring(objOffsetName,objOffsetVersion);
-    objfullVersion = objAgent.substring(objOffsetVersion+1);
-    if (objbrowserName.toLowerCase()==objbrowserName.toUpperCase()) {
-        objbrowserName = navigator.appName;
+    else if ((objOffsetName = objAgent.lastIndexOf(' ') + 1) <
+        (objOffsetVersion = objAgent.lastIndexOf('/'))) {
+        objbrowserName = objAgent.substring(objOffsetName, objOffsetVersion);
+        objfullVersion = objAgent.substring(objOffsetVersion + 1);
+        if (objbrowserName.toLowerCase() == objbrowserName.toUpperCase()) {
+            objbrowserName = navigator.appName;
+        }
     }
-}
 // In Opera
-else if ((objOffsetVersion=objAgent.indexOf("Opera"))!=-1) {
-    objbrowserName = "Opera";
-}
+    else if ((objOffsetVersion = objAgent.indexOf("Opera")) != -1) {
+        objbrowserName = "Opera";
+    }
 // trimming the fullVersion string at semicolon/space if present
-if ((ix=objfullVersion.indexOf(";"))!=-1)
-    objfullVersion=objfullVersion.substring(0,ix);
-if ((ix=objfullVersion.indexOf(" "))!=-1)
-    objfullVersion=objfullVersion.substring(0,ix);
+    if ((ix = objfullVersion.indexOf(";")) != -1)
+        objfullVersion = objfullVersion.substring(0, ix);
+    if ((ix = objfullVersion.indexOf(" ")) != -1)
+        objfullVersion = objfullVersion.substring(0, ix);
 
-objBrMajorVersion = parseInt(''+objfullVersion,10);
-if (isNaN(objBrMajorVersion)) {
-    objfullVersion  = ''+parseFloat(navigator.appVersion);
-    objBrMajorVersion = parseInt(navigator.appVersion,10);
-}
+    objBrMajorVersion = parseInt('' + objfullVersion, 10);
+    if (isNaN(objBrMajorVersion)) {
+        objfullVersion = '' + parseFloat(navigator.appVersion);
+        objBrMajorVersion = parseInt(navigator.appVersion, 10);
+    }
 // Android Mobile
-var isAndroidMobile = objAgent.indexOf('Android') > -1 && objAgent.indexOf('Mozilla/5.0') > -1 && objAgent.indexOf('AppleWebKit') > -1;
-var isIOSMobile = objAgent.indexOf('iPhone') > -1 && objAgent.indexOf('Mozilla/5.0') > -1 && objAgent.indexOf('AppleWebKit') > -1;
+    var isAndroidMobile = objAgent.indexOf('Android') > -1 && objAgent.indexOf('Mozilla/5.0') > -1 && objAgent.indexOf('AppleWebKit') > -1;
+    var isIOSMobile = objAgent.indexOf('iPhone') > -1 && objAgent.indexOf('Mozilla/5.0') > -1 && objAgent.indexOf('AppleWebKit') > -1;
 
-var isOperaMobile = objAgent.indexOf('Opera') > -1 && objAgent.indexOf('Android') > -1;
-if(isAndroidMobile || isOperaMobile){
-    var androidVersion = parseFloat(objAgent.slice(objAgent.indexOf("Android")+8));
-}
+    var isOperaMobile = objAgent.indexOf('Opera') > -1 && objAgent.indexOf('Android') > -1;
+    if (isAndroidMobile || isOperaMobile) {
+        var androidVersion = parseFloat(objAgent.slice(objAgent.indexOf("Android") + 8));
+    }
 // Android Browser (not Chrome)
-var regExAppleWebKit = new RegExp(/AppleWebKit\/([\d.]+)/);
-var resultAppleWebKitRegEx = regExAppleWebKit.exec(objAgent);
-var appleWebKitVersion = (resultAppleWebKitRegEx === null ? null : parseFloat(regExAppleWebKit.exec(objAgent)[1]));
-var isAndroidBrowser = isAndroidMobile && appleWebKitVersion !== null && appleWebKitVersion < 537;
-var isiOsBrowser = isIOSMobile && appleWebKitVersion !== null && appleWebKitVersion < 537;
+    var regExAppleWebKit = new RegExp(/AppleWebKit\/([\d.]+)/);
+    var resultAppleWebKitRegEx = regExAppleWebKit.exec(objAgent);
+    var appleWebKitVersion = (resultAppleWebKitRegEx === null ? null : parseFloat(regExAppleWebKit.exec(objAgent)[1]));
+    var isAndroidBrowser = isAndroidMobile && appleWebKitVersion !== null && appleWebKitVersion < 537;
+    var isiOsBrowser = isIOSMobile && appleWebKitVersion !== null && appleWebKitVersion < 537;
 
-lprint("Browser:" + resultAppleWebKitRegEx + " Agent:" + objAgent);
+    //lprint("Browser:" + resultAppleWebKitRegEx + " Agent:" + objAgent);
 
-if((objbrowserName == "Microsoft Internet Explorer" && objBrMajorVersion < 10)
-    || (objbrowserName == "Firefox" && objBrMajorVersion < 5)
-    || (objbrowserName == "Chrome" && objBrMajorVersion < 14)
-    || (objbrowserName == "Safari" && objBrMajorVersion < 5 && !isAndroidBrowser)
-    || (isAndroidBrowser && androidVersion < 4)
-    || (isiOsBrowser)
-    || ((objbrowserName == "Opera" || navigator.appName == "Opera") && objBrMajorVersion < 21)
-    || (!window.WebSocket)
-    || (!window.localStorage))
-{
-    $(document).ready(function(){
-        var pageSwitchBrowser = document.createElement('div');
-        pageSwitchBrowser.setAttribute("id",'overLayIncompatibleBrowser');
-        pageSwitchBrowser.innerHTML = '<div class="contentUnsupported">' +
-            '<div class="headerUnsupported">' +
-            '<label class="titleUnsupported">Improve Your Experience</label>' +
-            '</div>'+
-            '<div class="descriptionUnsuported"><label>You\'re using a web browser we don\'t support.</label><label>Try one of these options to access this aplication.</label></div>'+
-            '<ul class="optionList">' +
-            '<li class="itemsToPickFrom"><a href="http://www.google.ro/intl/ro/chrome/browser/"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/ChromeIcon.png" /><div class="labelBrowser"><label>Google Chrome</label></div></a></li>'+
-            '<li class="itemsToPickFrom"><a href="http://www.mozilla.org/ro/firefox/new/"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/firefox-icon.png"/><div class="labelBrowser"><label>Mozilla Firefox</label></div></a></li>'+
-            '<li class="itemsToPickFrom"><a href="http://www.opera.com/"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/OperaIcon.png"/><div class="labelBrowser"><label>Opera</label></div></a></li>'+
-            '<li class="itemsToPickFrom"><a href="http://support.apple.com/downloads/#safari"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/SafariIcon.png"/><div class="labelBrowser"><label>Safari</label></div></a></li>'+
-            '<li class="itemsToPickFrom"><a href="http://windows.microsoft.com/ro-ro/internet-explorer/download-ie"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/InternetExplorerIcon.png"/><div class="labelBrowser"><label>Internet Explorer</label></div></a></li>'+
-            '</ul>'+
-            '</div>';
-        if((isAndroidBrowser && androidVersion < 4) || (isOperaMobile && objBrMajorVersion < 21 && androidVersion < 4)){
+    if ((objbrowserName == "Microsoft Internet Explorer" && objBrMajorVersion < 10)
+        || (objbrowserName == "Firefox" && objBrMajorVersion < 5)
+        || (objbrowserName == "Chrome" && objBrMajorVersion < 14)
+        || (objbrowserName == "Safari" && objBrMajorVersion < 5 && !isAndroidBrowser)
+        || (isAndroidBrowser && androidVersion < 4)
+        || (isiOsBrowser)
+        || ((objbrowserName == "Opera" || navigator.appName == "Opera") && objBrMajorVersion < 21)
+        || (!window.WebSocket)
+        || (!window.localStorage)) {
+        $(document).ready(function () {
+            var pageSwitchBrowser = document.createElement('div');
+            pageSwitchBrowser.setAttribute("id", 'overLayIncompatibleBrowser');
             pageSwitchBrowser.innerHTML = '<div class="contentUnsupported">' +
                 '<div class="headerUnsupported">' +
                 '<label class="titleUnsupported">Improve Your Experience</label>' +
-                '</div>'+
-                '<div class="descriptionUnsuported"><label>You\'re using a web browser we don\'t support.</label><label>Try one of these options to access this aplication.</label></div>'+
-                '<ul class="optionList" style="width: 300px">' +
-                '<li class="itemsToPickFrom" style="display: block"><a href="https://play.google.com/store/apps/details?id=org.mozilla.firefox"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/firefox-icon.png"/><div class="labelBrowser"><label>Mozilla Firefox</label></div></a></li>'+
-                '</ul>'+
+                '</div>' +
+                '<div class="descriptionUnsupported"><label>You\'re using a web browser we don\'t support.</label><label>Try one of these options to access this application.</label></div>' +
+                '<ul class="optionList">' +
+                '<li class="itemsToPickFrom"><a href="http://www.google.ro/intl/ro/chrome/browser/"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/ChromeIcon.png" /><div class="labelBrowser"><label>Google Chrome</label></div></a></li>' +
+                '<li class="itemsToPickFrom"><a href="http://www.mozilla.org/ro/firefox/new/"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/firefox-icon.png"/><div class="labelBrowser"><label>Mozilla Firefox</label></div></a></li>' +
+                '<li class="itemsToPickFrom"><a href="http://www.opera.com/"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/OperaIcon.png"/><div class="labelBrowser"><label>Opera</label></div></a></li>' +
+                '<li class="itemsToPickFrom"><a href="http://support.apple.com/downloads/#safari"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/SafariIcon.png"/><div class="labelBrowser"><label>Safari</label></div></a></li>' +
+                '<li class="itemsToPickFrom"><a href="http://windows.microsoft.com/ro-ro/internet-explorer/download-ie"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/InternetExplorerIcon.png"/><div class="labelBrowser"><label>Internet Explorer</label></div></a></li>' +
+                '</ul>' +
                 '</div>';
-        }
-        document.getElementsByTagName("body")[0].appendChild(pageSwitchBrowser);
-    })
-
-
+            if ((isAndroidBrowser && androidVersion < 4) || (isOperaMobile && objBrMajorVersion < 21 && androidVersion < 4)) {
+                pageSwitchBrowser.innerHTML = '<div class="contentUnsupported">' +
+                    '<div class="headerUnsupported">' +
+                    '<label class="titleUnsupported">Improve Your Experience</label>' +
+                    '</div>' +
+                    '<div class="descriptionUnsupported"><label>You\'re using a web browser we don\'t support.</label><label>Try one of these options to access this aplication.</label></div>' +
+                    '<ul class="optionList" style="width: 300px">' +
+                    '<li class="itemsToPickFrom" style="display: block"><a href="https://play.google.com/store/apps/details?id=org.mozilla.firefox"><img class="imgBrowserIcon" src="../SwarmShape/deps/img/firefox-icon.png"/><div class="labelBrowser"><label>Mozilla Firefox</label></div></a></li>' +
+                    '</ul>' +
+                    '</div>';
+            }
+            document.getElementsByTagName("body")[0].appendChild(pageSwitchBrowser);
+        })
+    }
 }
+
 
 
---
+++ deps/phoneGapCode.js
@@ -0,0 +1,69 @@
+
+var objAgent = navigator.userAgent;
+appPhoneGap = document.URL.indexOf( 'http://' ) === -1 && document.URL.indexOf( 'https://' ) === -1;
+if ( appPhoneGap ) {
+    // PhoneGap application
+    $(document).ready(function () {
+        var cordovaScript=document.createElement('script');
+        cordovaScript.setAttribute("type","text/javascript");
+        cordovaScript.setAttribute("src", 'cordova.js');
+        document.getElementsByTagName("head")[0].appendChild(cordovaScript);
+        var facebookPluginScript=document.createElement('script');
+        facebookPluginScript.setAttribute("type","text/javascript");
+        facebookPluginScript.setAttribute("src", 'cdv-plugin-fb-connect.js');
+        document.getElementsByTagName("head")[0].appendChild(facebookPluginScript);
+        var facebookSDKScript=document.createElement('script');
+        facebookSDKScript.setAttribute("type","text/javascript");
+        facebookSDKScript.setAttribute("src", 'facebook-js-sdk.js');
+        document.getElementsByTagName("head")[0].appendChild(facebookSDKScript);
+        var pushNotificationScript=document.createElement('script');
+        pushNotificationScript.setAttribute("type","text/javascript");
+        pushNotificationScript.setAttribute("src", 'PushNotification.js');
+        document.getElementsByTagName("head")[0].appendChild(pushNotificationScript);
+    });
+// Android Mobile
+    var isAndroidMobile = objAgent.indexOf('Android') > -1 && objAgent.indexOf('Mozilla/5.0') > -1 && objAgent.indexOf('AppleWebKit') > -1;
+    if (isAndroidMobile) {
+        var androidVersion = parseFloat(objAgent.slice(objAgent.indexOf("Android") + 8));
+    }
+    if (isAndroidMobile && androidVersion < 4) {
+        $(document).ready(function () {
+            var siteLink = "http://192.168.2.140/channels";
+            var marketFirefoxLink = "https://play.google.com/store/apps/details?id=org.mozilla.firefox";
+            var pageSwitchBrowser = document.createElement('div');
+            pageSwitchBrowser.setAttribute("id", 'overLayIncompatibleBrowser');
+            pageSwitchBrowser.innerHTML = '<div class="contentUnsupported">' +
+                '<div class="headerUnsupported">' +
+                '<label class="titleUnsupported">Improve Your Experience</label>' +
+                '</div>' +
+                '<div class="descriptionUnsupported"><label>You\'re using a Android version we don\'t support.</label><label>You can access this application from web by tapping the follow icon, we strongly recommend Firefox Browser.</label></div>' +
+                '<ul class="optionList" style="width: 300px">' +
+                '<li class="itemsToPickFrom" style="display: block"><a href="#null" id="siteLink" target="_system"><img class="imgBrowserIcon" src="img/channel64.png"/><div class="labelBrowser"><label>TheBuzzCenter</label></div></a></li>' +
+                '</ul>' +
+                '<div class="descriptionUnsupported"><label>If you don\'t have Firefox browser installed on your device tap on the follow icon.</label></div>' +
+                '<ul class="optionList" style="width: 300px">' +
+                '<li class="itemsToPickFrom" style="display: block"><a href="#null" id="marketFirefoxLink" target="_system" ><img class="imgBrowserIcon" src="img/firefox-icon.png"/><div class="labelBrowser"><label>Mozilla Firefox</label></div></a></li>' +
+                '</ul>' +
+                '</div>';
+            document.getElementsByTagName("body")[0].appendChild(pageSwitchBrowser);
+            document.getElementById("siteLink").addEventListener("click", function () {
+                openExternalLink(siteLink);
+            }, false);
+            document.getElementById("marketFirefoxLink").addEventListener("click", function () {
+                openExternalLink(marketFirefoxLink);
+            }, false);
+            function openExternalLink(link) {
+                //alert("openExternalLink "+link);
+                navigator.app.loadUrl(link, { openExternal: true });
+                return false;
+            }
+        })
+    }
+    function onDeviceReady() {
+        navigator.splashscreen.hide();
+
+    }
+    document.addEventListener("deviceready", onDeviceReady, false);
+
+}
+
